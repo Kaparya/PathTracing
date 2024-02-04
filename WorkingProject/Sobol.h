af03a6ff4ee34250f7da6cea98899fa4ffb08cb5
Damir Kandelov <kaparyaka@mail.ru>
2024-02-04 22:30:58 +0300

    Sobol's random generator ready

--- WorkingProject/Sobol.h
+++ WorkingProject/Sobol.h
@@ -4,11 +4,13 @@ static constexpr int SobolMatrixSize = 52;
 extern const uint32_t SobolMatrices32[NumSobolDimensions *
                                       SobolMatrixSize];
 
-inline float SobolRand(int64_t index, int dimension,
-                       uint64_t scramble = 0) {
+inline float SobolRand(uint32_t index, uint32_t dimension,
+                       uint32_t scramble = 0) {
     uint32_t v = scramble;
-    for (int i = dimension * SobolMatrixSize; index != 0; index >>= 1, ++i)
-        if (index & 1)
+    for (uint32_t i = dimension * SobolMatrixSize; index != 0; index >>= 1, ++i) {
+        if (index & 1) {
             v ^= SobolMatrices32[i];
+        }
+    }
     return v * 0x1p-32f; /* 1/2^32 */
 }
