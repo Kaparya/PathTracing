7c5963ba969ec19083ca9fed4c4bb5273be3bf8b
Damir Kandelov <kaparyaka@mail.ru>
2024-02-09 20:27:00 +0300

    Upgrade scatters builder

--- MakeScatterForPixel/main.py
+++ MakeScatterForPixel/main.py
@@ -1,33 +1,72 @@
-
 import matplotlib.pyplot as plt
+
+
+fig, axs = plt.subplots(1, 3, dpi=150)
+fig.suptitle("Dimensions Test")
+fig.tight_layout()
 
 coords_x = []
 coords_y = []
-
-gen = 2
-
-if gen == 0:
-    x_file_name = "ePixelX_0.txt"
-    y_file_name = "ePixelY_1.txt"
-elif gen == 1:
-    x_file_name = "eLightPointX_3.txt"
-    y_file_name = "eLightPointY_4.txt"
-else:
-    x_file_name = "eGetRayX_9.txt"
-    y_file_name = "eGetRayY_10.txt"
-
-data_x = open("/Users/kaparya/HSE/PathTracing/WorkingProject/DimensionsTest/" + x_file_name, "r")
+data_x = open("/Users/kaparya/HSE/PathTracing/WorkingProject/DimensionsTest/ePixelX_0.txt", "r")
 for line in data_x:
     help = line.split()
     coords_x.append(float(help[0]))
 
-data_y = open("/Users/kaparya/HSE/PathTracing/WorkingProject/DimensionsTest/" + y_file_name, "r")
+data_y = open("/Users/kaparya/HSE/PathTracing/WorkingProject/DimensionsTest/ePixelY_1.txt", "r")
 for line in data_y:
     help = line.split()
     coords_y.append(float(help[0]))
 
-plt.scatter(coords_x, coords_y, s=3)
-plt.xlim(0, 1)
-plt.ylim(0, 1)
+axs[0].scatter(coords_x, coords_y, s=3)
+axs[0].set_title("ePixel (0/1 dim)")
+axs[0].set_xlim(0, 1)
+axs[0].set_xlabel("0 dim")
+axs[0].set_ylim(0, 1)
+axs[0].set_ylabel("1 dim")
+axs[0].set_box_aspect(1)
 
+
+coords_x.clear()
+coords_y.clear()
+data_x = open("/Users/kaparya/HSE/PathTracing/WorkingProject/DimensionsTest/eLightPointX_3.txt", "r")
+for line in data_x:
+    help = line.split()
+    coords_x.append(float(help[0]))
+
+data_y = open("/Users/kaparya/HSE/PathTracing/WorkingProject/DimensionsTest/eLightPointY_4.txt", "r")
+for line in data_y:
+    help = line.split()
+    coords_y.append(float(help[0]))
+
+axs[1].scatter(coords_x, coords_y, s=3)
+axs[1].set_title("eLightPoint (3/4 dim)")
+axs[1].set_xlim(0, 1)
+axs[1].set_xlabel("3 dim")
+axs[1].set_ylim(0, 1)
+axs[1].set_ylabel("4 dim")
+axs[1].set_box_aspect(1)
+
+
+coords_x.clear()
+coords_y.clear()
+data_x = open("/Users/kaparya/HSE/PathTracing/WorkingProject/DimensionsTest/eGetRayX_9.txt", "r")
+for line in data_x:
+    help = line.split()
+    coords_x.append(float(help[0]))
+
+data_y = open("/Users/kaparya/HSE/PathTracing/WorkingProject/DimensionsTest/eGetRayY_10.txt", "r")
+for line in data_y:
+    help = line.split()
+    coords_y.append(float(help[0]))
+
+axs[2].scatter(coords_x, coords_y, s=3)
+axs[2].set_title("eGetRay (9/10 dim)")
+axs[2].set_xlim(0, 1)
+axs[2].set_xlabel("9 dim")
+axs[2].set_ylim(0, 1)
+axs[2].set_ylabel("10 dim")
+axs[2].set_box_aspect(1)
+
+
+plt.savefig("Halton/100_8.png", bbox_inches='tight')
 plt.show()
